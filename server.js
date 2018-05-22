c466012c755dc8958d8d40bccb87f6b8c83b11df
xXFoxChrysalisXx <38595488+xXFoxChrysalisXx@users.noreply.github.com>
2018-05-22 10:52:58 -0400

    Update server.js

--- server.js
+++ server.js
@@ -76,7 +76,11 @@ app.post('/recipes', jsonParser, (req, res) => {
 app.get('/recipes', (req, res) => {
   res.json(Recipes.get());
 })
-
+app.delete('/recipes/:id', (req, res) => {
+  Recipes.delete(req.params.id);
+  console.log(`Deleted recipe item \`${req.params.id}\``);
+  res.status(204).end();
+});
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 });
